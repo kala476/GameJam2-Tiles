1c4937daa787367d911b5b2dd8a81cc9d2e28996
sjoylaube <joy.laube@outlook.de>
2020-07-10 12:19:00 +0200

    random tile replacement on enabling tileContainer

---
+++ Tiles/Assets/TileDistributor.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileDistributor : MonoBehaviour
+{
+    [SerializeField] List<GameObject> tilePrefabs;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    private void OnEnable()
+    {
+        ReplaceTiles();
+    }
+
+    private void ReplaceTiles()
+    {
+        int childCount = this.transform.childCount;
+        for (int i = 0; i < childCount; i++) 
+        {
+            Transform child = this.transform.GetChild(i);
+            Vector3 childPosition = child.transform.position;
+            GameObject newTile = tilePrefabs[UnityEngine.Random.Range(0, tilePrefabs.Count)];
+            Destroy(child.gameObject);
+            Instantiate<GameObject>(newTile, childPosition, newTile.transform.rotation, this.transform);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
